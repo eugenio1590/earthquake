57eaa6f339a2a782fbd2fa7b1ce9d35929e9dff5
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-04-05 21:15:17 -0500

    Earthquakes component

---
+++ app/javascript/bundles/Earthquakes/components/Earthquakes.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Earthquakes = () => {
+  const [name, setName] = useState('Stranger');
+
+  return (
+    <div>
+      <h3>Hello, {name}!</h3>
+      <hr />
+      <form>
+        <label htmlFor="name">
+          Say hello to:
+          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default Earthquakes;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,12 @@
 //
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
+
+import ReactOnRails from 'react-on-rails';
+
+import Earthquakes from '../bundles/Earthquakes/components/Earthquakes';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  Earthquakes,
+});
