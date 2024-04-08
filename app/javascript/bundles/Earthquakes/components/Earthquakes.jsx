27686bef8282fb300e6c385f7a6134e816f35051
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-04-08 10:10:53 -0500

    mui

--- app/javascript/bundles/Earthquakes/components/Earthquakes.jsx
+++ app/javascript/bundles/Earthquakes/components/Earthquakes.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
+import { Box, InputLabel, TextField, Divider, Typography } from "@mui/material";
 
 const Earthquakes = () => {
   const [name, setName] = useState('Stranger');
 
   return (
-    <div>
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-      </form>
-    </div>
+    <Box>
+      <Typography variant='h5'>Hello, {name}!</Typography>
+      <Divider sx={{marginTop: 3, marginBottom: 3}} />
+      <InputLabel htmlFor="name">Say hello to:</InputLabel>
+      <TextField id="name" value={name} onChange={(e) => setName(e.target.value)} />
+    </Box>
   );
 };
 
